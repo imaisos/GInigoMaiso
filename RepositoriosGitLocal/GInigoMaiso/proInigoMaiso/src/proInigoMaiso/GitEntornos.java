fbc80e26ae4adabddcb4e00bf9547c56a1eefa0d
imaisos <78826280+imaisos@users.noreply.github.com>
2021-05-24 12:25:40 +0200

    Update GitEntornos.java

--- RepositoriosGitLocal/GInigoMaiso/proInigoMaiso/src/proInigoMaiso/GitEntornos.java
+++ RepositoriosGitLocal/GInigoMaiso/proInigoMaiso/src/proInigoMaiso/GitEntornos.java
@@ -3,6 +3,6 @@ package proInigoMaiso;
 public class GitEntornos {
 	public static void main (String[] args) {
 		System.out.println("Esto es una prueba en Git");
-		int anio = 2005;
+		int anio = 2001;
 	}
 }
