b76361bfe50a7430369c53e689ff58158111efd0
imaiso <imaisojudo@gmail.com>
2021-05-24 12:44:24 +0200

    Update GitEntornos.java

--- RepositoriosGitLocal/GInigoMaiso/proInigoMaiso/src/proInigoMaiso/GitEntornos.java
+++ RepositoriosGitLocal/GInigoMaiso/proInigoMaiso/src/proInigoMaiso/GitEntornos.java
@@ -4,5 +4,6 @@ public class GitEntornos {
 	public static void main (String[] args) {
 		System.out.println("Esto es una prueba en Git");
 		int anio = 2001;
+		System.out.println("Hola soy el 2º usuario");
 	}
 }
